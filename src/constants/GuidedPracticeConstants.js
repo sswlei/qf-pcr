c525112ce9b9c965b2b6c001ee4852d76e6a70d8
Sharon Lei <sharon_lei@bcit.ca>
2021-12-07 17:24:32 -0800

    generate list of dropdowns based on marker answers data

---
+++ src/constants/GuidedPracticeConstants.js
@@ -0,0 +1,18 @@
+export var Chromosome = {
+    ch13:{name:"Ch13",value:"ch13"},
+    ch18:{name:"Ch18",value:"ch18"},
+    ch21:{name:"Ch21",value:"ch21"},
+    chx:{name:"Ch X",value:"chx"},
+    chy:{name:"Ch Y",value:"chy"}
+};
+export var Allele = {
+    uninformative:{name:"Uninformative",value:"uninformative"},
+    diallelic:{name:"Diallelic",value:"diallelic"},
+    triallelic:{name:"Triallelic",value:"triallelic"}
+}
+export var SelectionType = {
+    chromOnly:"chromOnly",
+    chromAndAllele:"chromAndAllele",
+    amel:"amel",
+    taf9l:"taf9l"
+}
--- src/pages/GuidedPractice/PrenatalRadDemo.js
+++ src/pages/GuidedPractice/PrenatalRadDemo.js
@@ -1,64 +1,128 @@
 import React, { Component } from 'react';
 import NormalMale from '../../assets/normalMale.JPG';
 import ReactImageMagnify from 'react-image-magnify';
-import { SplitButton, Dropdown, Button, ButtonGroup, Card, Row, Container } from 'react-bootstrap';
+import { DropdownButton, Dropdown, Button, ButtonGroup, Card, Row, Container } from 'react-bootstrap';
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
-
+import { Chromosome, Allele, SelectionType } from "../../constants/GuidedPracticeConstants"
 class PrenatalRadDemo extends Component {
 
     constructor(props) {
         super(props);
+        this.markerData = [
+            {
+                "id":"DXS6803",
+                "type":SelectionType.chromOnly,
+                "chrom_answer":Chromosome.chx.value,
+                "user_selected_chrom":""
+            },
+            {
+                "id":"D21S1435",
+                "type":SelectionType.chromAndAllele,
+                "chrom_answer":Chromosome.ch21.value,
+                "allele_answer":Allele.diallelic
+            },
+            {
+                "id":"D21S11",
+                "type":SelectionType.chromAndAllele,
+                "chrom_answer":Chromosome.ch21.value,
+                "allele_answer":Allele.uninformative
+            },
+            {
+                "id":"D21S1437",
+                "type":SelectionType.chromAndAllele,
+                "chrom_answer":Chromosome.ch21.value,
+                "allele_answer":Allele.diallelic
+            },
+            {
+                "id":"D13S634",
+                "type":SelectionType.chromAndAllele,
+                "chrom_answer":Chromosome.ch13.value,
+                "allele_answer":Allele.diallelic
+            },
+            {
+                "id":"D18S535",
+                "type":SelectionType.chromAndAllele,
+                "chrom_answer":Chromosome.ch18.value,
+                "allele_answer":Allele.diallelic
+            }
+        ]
+        this.getDropdowns = this.getDropdowns.bind(this);
     }
+
+    getDropdowns(marker){
+        if (marker.type==="chromAndAllele"){
+            return <div className="mb-3">
+                <Button variant="secondary">{marker.id}</Button>
+                <Dropdown className="mt-3 d-inline" as={ButtonGroup}>
+                    <label className="mx-0 my-0 px-4 py-0" style={{border:"1px solid gray",lineHeight:"38px"}}>Select</label>
+                    <Dropdown.Toggle/>
+                    <Dropdown.Menu alignRight>
+                        <Dropdown.Item>{Chromosome.ch13.name}</Dropdown.Item>
+                        <Dropdown.Item>{Chromosome.ch18.name}</Dropdown.Item>
+                        <Dropdown.Item>{Chromosome.ch21.name}</Dropdown.Item>
+                        <Dropdown.Item>{Chromosome.chx.name}</Dropdown.Item>
+                        <Dropdown.Item>{Chromosome.chy.name}</Dropdown.Item>
+
+                    </Dropdown.Menu>
+                </Dropdown> 
+                <Dropdown className="mt-3 d-inline" as={ButtonGroup}>
+                    <label className="mx-0 my-0 px-4 py-0" style={{border:"1px solid gray",lineHeight:"38px"}}>Select</label>
+                    <Dropdown.Toggle/>
+                    <Dropdown.Menu alignRight>
+                        <Dropdown.Item>{Allele.uninformative.name}</Dropdown.Item>
+                        <Dropdown.Item>{Allele.diallelic.name}</Dropdown.Item>
+                        <Dropdown.Item>{Allele.triallelic.name}</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown> 
+            </div>
+        }
+        if (marker.type==="amel"){
+
+        }
+        if (marker.type==="chromOnly"){
+            return <div className="mb-3">
+                <Button variant="secondary">{marker.id}</Button>
+                <Dropdown className="mt-3 d-inline" as={ButtonGroup}>
+                    <label className="mx-0 my-0 px-4 py-0" style={{border:"1px solid gray",lineHeight:"38px"}}>Select</label>
+                    <Dropdown.Toggle/>
+                    <Dropdown.Menu alignRight>
+                        <Dropdown.Item>{Chromosome.ch13.name}</Dropdown.Item>
+                        <Dropdown.Item>{Chromosome.ch18.name}</Dropdown.Item>
+                        <Dropdown.Item>{Chromosome.ch21.name}</Dropdown.Item>
+                        <Dropdown.Item>{Chromosome.chx.name}</Dropdown.Item>
+                        <Dropdown.Item>{Chromosome.chy.name}</Dropdown.Item>
+
+                    </Dropdown.Menu>
+                </Dropdown> 
+            </div>
+        }
+        if (marker.type==="taf9l"){
+
+        }
+    }
+    
 
     render() {
 
-
         return (
             <Container>
-                {/* <ReactImageMagnify
-                    className="col-8"
-                    smallImage={{
-                        alt: 'Normal Male',
-                        src: NormalMale,
-                        isFluidWidth: true,
-                    }}
-                    largeImage={{
-                        src:  NormalMale,
-                        width: 2000,
-                        height: 800,
-                    }}
-                    enlargedImagePosition="over"
-
-                    
-                /> */}
                 <Row>
-                <Card className="col-9">
+                <Card className="col-7">
                     <Card.Body>
-                        <TransformWrapper initialScale={0.4} minScale={0.4} maxScale={2} centerOnInit={true}>
+                        <TransformWrapper initialScale={0.3} minScale={0.3} maxScale={2} centerOnInit={true}>
                             <TransformComponent wrapperStyle={{width:"100%",height:500}}>
                                 <img src={NormalMale} alt="Normal Male" />
                             </TransformComponent>
                         </TransformWrapper>
                     </Card.Body>
                 </Card>
-                <Card  className="col-3">
+                <Card  className="col-5">
                     <Card.Body>
-                    <SplitButton menuAlign="start" bsStyle="info" bsSize="large" title="Main List">
-                    <Dropdown.Item eventKey="1">Action</Dropdown.Item>
-                                <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
-</SplitButton>
-                        <Dropdown as={ButtonGroup}>
-                            <Button variant="secondary">DXS6803</Button>
-                            <label className="px-3">Selected</label>
-                            <Dropdown.Toggle id="" />
-                            <Dropdown.Menu align="start">
-                                <Dropdown.Item eventKey="1">Action</Dropdown.Item>
-                                <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
-                                <Dropdown.Item eventKey="3" active>
-                                    Active Item
-                                </Dropdown.Item>
-                            </Dropdown.Menu>
-                        </Dropdown>
+                        {this.markerData.map(function(marker, i){
+                            return <div>
+                                {this.getDropdowns(marker)}
+                            </div>
+                        },this)}
                     </Card.Body>
 
                 </Card>
