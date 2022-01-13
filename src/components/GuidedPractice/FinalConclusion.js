8c5f5d49a5f1362f51c0b8ffeeabcc6d1f54bb50
Sharon Lei <sharon_lei@bcit.ca>
2022-01-12 16:56:00 -0800

    edits to text in final conclusion

--- src/components/GuidedPractice/FinalConclusion.js
+++ src/components/GuidedPractice/FinalConclusion.js
@@ -5,24 +5,33 @@ class FinalConclusion extends Component{
     render(){
         return <div>
             <h3>Is there enough evidence for a final conclusion?</h3>
+            <br></br>
             <p>
-                Normal: must have at least 2 markers for each chromosome that have normal ratio: all others uninformative
+                Based on what we have observed from the previous two steps, what can we conclude about the subject?
+                <br></br>
+                <br></br>
+                <strong>Normal:</strong> must have at least 2 markers for each chromosome that have normal ratio: all others uninformative
                 <br></br>
                 <br></br>
 
-                Trisomy: for affected chromosome: must have at least 2 markers for chromosome that have trisomy ratio, or 3 peaks: all others uninformative
+                <strong>Trisomy:</strong>for affected chromosome: must have at least 2 markers for chromosome that have trisomy ratio, or 3 peaks: all others uninformative
                 If both normal and abnormal allele patterns seen, call for further testing
                 <br></br>
                 <br></br>
-                Triploidy (3N): suspected if all chromosomes show trisomy (for this demo, select Other as conclusion)
+                <strong>Triploidy (3N):</strong> suspected if all chromosomes show trisomy (for this demo, select Other as conclusion)
                 <br></br>
                 <br></br>
 
                 <h5>Sex chromosome abnormalities</h5>
-                Klinefelters: need AMEL XY, one peak for Ch Y loci, 2 peaks or uninformative for Ch X loci, TAF9L ratio is for 2X (i.e. Ch 3 and Ch X peaks same peak area)
+                <br></br>
+
+                <strong>Klinefelters:</strong> need AMEL XY, one peak for Ch Y loci, 2 peaks or uninformative for Ch X loci, TAF9L ratio is for 2X (i.e. Ch 3 and Ch X peaks same peak area)
                 <br></br>
                 <br></br>
-                Turner: need AMEL XX, no ChY loci peaks, 1 peak for Ch X loci, TAF9L ratio is for 1X (i.e. Ch 3 has peak area twice Ch X peak area (remember to also review autosomes: there can be more than only abnormality (but this is rare) 
+                <strong>Turner:</strong> need AMEL XX, no ChY loci peaks, 1 peak for Ch X loci, TAF9L ratio is for 1X (i.e. Ch 3 has peak area twice Ch X peak area)
+                <br></br>
+                <br></br>
+                Remember to also review autosomes: there can be more than only abnormality (but this is rare) 
                 <br></br>
                 <br></br>
                 Select what we can conclude about the subject:
