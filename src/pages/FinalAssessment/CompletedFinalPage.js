06b286a1ef70463211b3b096ca2efca0665ca380
Sharon Lei <sharon_lei@bcit.ca>
2022-04-22 20:17:04 -0700

    add final assessment completion page, encrypt completion data, add achievement for final assessment

--- src/pages/FinalAssessment/CompletedFinalPage.js
+++ src/pages/FinalAssessment/CompletedFinalPage.js
@@ -2,17 +2,34 @@ import { Component } from "react";
 import { Image, Card, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import trophy from "../../assets/winner.png";
-
+import { enc, AES } from "crypto-js";
 class CompletedFinalAssessmentPage extends Component{
     constructor(props) {
         super(props);
+        this.state = {completionData:{}};
+        let storage_data = localStorage.getItem("final_assessment_completion");
+        if (storage_data==null){
+            this.props.history.push(`/`);
+        }
+        else{
+            let completion_data = AES.decrypt(storage_data,"9g92m498dh4sx").toString(enc.Utf8);
+            console.log(completion_data);
+            try{
+                let decryptedData = JSON.parse(completion_data);
+                console.log(decryptedData);
+                this.state = {completionData:decryptedData};
+            }
+            catch (e){
+                this.props.history.push(`/`);
+            }
+        }
     }
 
     render(){ 
         return <Container className="mt-4">
             <Card className="px-5 py-5 mb-5" style={{textAlign:"center"}}>
                 <Card.Title>
-                    <h2>Congratulations for completing this online QF-PCR practice on {(new Date()).toLocaleDateString("en-US")}</h2>
+                    <h2>Congratulations for completing this online QF-PCR practice on {this.state.completionData.date}</h2>
                 </Card.Title>
                 <Card.Body>
                     <Image height={200} src={trophy}></Image>
