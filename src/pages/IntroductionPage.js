45af4fcfd341c3e779acc60f41fc615de383dca6
Sharon Lei <sharon_lei@bcit.ca>
2022-07-13 15:26:46 -0700

    Wording updates for May 3rd edits

--- src/pages/IntroductionPage.js
+++ src/pages/IntroductionPage.js
@@ -17,7 +17,7 @@ class IntroductionPage extends Component {
 
                 <div style={{ fontSize: '1.2rem', height: '100%' }} className="d-flex col-sm-11 flex-column align-items-start bg-white ">
                     <h3 className="text-info text-monospace" style={{ letterSpacing: 1 }}>Introduction:</h3>
-                    <p className="mt-2">Welcome to the QF-PCR online learning module. This module will review some of the basic principles of this laboratory method for the detection of aneuploid in prenatal and pregnancy loss cases. Select your choices from the menu above to learn about the background, work through a guided practice case, take a quiz to test your knowledge, and try some additional practice cases.</p>
+                    <p className="mt-2">Welcome to the QF-PCR online learning module. This module will review some of the basic principles of this laboratory method for the detection of aneuploidy in prenatal and pregnancy loss cases. Select your choices from the menu above to learn about the background, work through a guided practice case, take a quiz to test your knowledge, and try some additional practice cases.</p>
                 </div>
                 <span className="d-flex flex-column col-sm-11 mt-3 align-items-center">
                     <Image
